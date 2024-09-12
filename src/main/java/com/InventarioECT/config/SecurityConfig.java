01309d68b2c9ad1b44d96acd39b5d046422cc484
Manuel Ortiz <manuel.ortiz@tecnomusic.local>
2024-09-12 17:31:00 -0500

    Implementacion nuevo metodo

--- src/main/java/com/InventarioECT/config/SecurityConfig.java
+++ src/main/java/com/InventarioECT/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.InventarioECT.config.filters.JwtAuthFilter;
 import com.InventarioECT.config.jwt.JwtUtils;
 import com.InventarioECT.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
